dd7165feab8b543ab66139c812e74f8df574bf66
fatemesoleymanian <71789169+fatemesoleymanian@users.noreply.github.com>
2020-09-30 18:41:19 -0700

    commit

---
+++ Homework/S(0)/Ex1/src/usc/ac/ir/Main.java
@@ -0,0 +1,12 @@
+package usc.ac.ir;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for(int i=1; i<5 ;i++ ){
+            for(int j=0; j<i ;j++){
+                System.out.print(" * ");
+            }System.out.println("");
+        }
+    }
+}
