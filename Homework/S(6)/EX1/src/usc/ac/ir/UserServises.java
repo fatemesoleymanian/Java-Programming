730c9ef9a236559cbe5c2601d817667976cfe826
fatemesoleymanian <71789169+fatemesoleymanian@users.noreply.github.com>
2020-11-14 15:43:35 -0800

    commit

--- Homework/S(6)/EX1/src/usc/ac/ir/UserServises.java
+++ Homework/S(6)/EX1/src/usc/ac/ir/UserServises.java
@@ -17,43 +17,35 @@ public class UserServises {
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
             Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hanie",
-                    "hjh79");
+                    "h13");
             PreparedStatement preparedStatement =
-                    connection.prepareStatement("insert into UserServices (user_name,e-mail,password,age,education) values (?,?,?,?,?)");
-
+                    connection.prepareStatement("insert into UserServices (user_code,user_name,email,password,age,education) values (?,?,?,?,?,?)");
+            System.out.println("user code :");
+            this.userCode=scanner.nextLong();
+            preparedStatement.setLong(1,this.userCode );
             System.out.println("user name :");
             this.userName=scanner.next();
-            preparedStatement.setString(1,this.userName );
+            preparedStatement.setString(2,this.userName );
             System.out.println("email :");
             this.email=scanner.next();
-            preparedStatement.setString(2,this.email );
+            preparedStatement.setString(3,this.email );
             System.out.println("password :");
             this.password=scanner.next();
-            preparedStatement.setString(3,this.password );
+            preparedStatement.setString(4,this.password );
             System.out.println("age :");
             this.age=scanner.nextInt();
-            preparedStatement.setInt(4,this.age );
+            preparedStatement.setInt(5,this.age );
             System.out.println("education :");
-            this.education=scanner.nextLine();
-            preparedStatement.setString(5,this.education );
+            this.education=scanner.next();
+            preparedStatement.setString(6,this.education );
             preparedStatement.executeUpdate();
-            PreparedStatement preparedStatement2 =
-                    connection.prepareStatement("select user_code from UserServices where user_name=? and password=? and e-mail=? and age=? and education=?");
 
-            preparedStatement2.setString(1,this.userName );
-            preparedStatement2.setString(2,this.password );
-            preparedStatement2.setString(3, this.email);
-            preparedStatement2.setInt(4, this.age);
-            preparedStatement2.setString(5, this.education);
-
-            ResultSet resultSet = preparedStatement2.executeQuery();
-            this.userCode=resultSet.getLong("user_code");
             preparedStatement.close();
             connection.close();
             System.out.println("You Successfully Signed up!");
         } catch (ClassNotFoundException e)
         {
-            System.out.println("DB Driver Not Exist!!");//not found the user?
+            System.out.println("DB Driver Not Exist!!");
         } catch (SQLException e)
         {
             System.out.println("DB ERROR " + e.getMessage());
@@ -74,15 +66,15 @@ public class UserServises {
         System.out.println("password :");
         this.password=scanner.next();
         System.out.println("age :");
-        this.age= Integer.parseInt(scanner.nextLine());
+        this.age= scanner.nextInt();
         System.out.println("education :");
-        this.education=scanner.nextLine();
+        this.education=scanner.next();
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
             Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hanie",
-                    "hjh79");
+                    "h13");
             PreparedStatement preparedStatement =
-                    connection.prepareStatement("update UsersServices set user_name=? ,e-mai=? ,password=? ,age=? ,education=? where user_code=?");
+                    connection.prepareStatement("update UserServices set user_name=? ,email=? ,password=? ,age=? ,education=? where user_code=?");
 
             preparedStatement.setString(1, this.userName);
 
@@ -114,16 +106,18 @@ public class UserServises {
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
             Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hanie",
-                    "hjh79");
+                    "h13");
             PreparedStatement preparedStatement = connection.prepareStatement("select * from UserServices where user_code=?");
             preparedStatement.setLong(1,this.userCode);
             ResultSet resultSet = preparedStatement.executeQuery();
-            System.out.println("user code: "+resultSet.getLong("user_code"));
-            System.out.println("user name: "+resultSet.getString("user_name"));
-            System.out.println("email address: "+resultSet.getString("e-mail"));
-            System.out.println("password: "+resultSet.getString("password"));
-            System.out.println("age: "+resultSet.getInt("age"));
-            System.out.println("education: "+resultSet.getString("education"));
+            while (resultSet.next()) {
+                System.out.println("user code: " + resultSet.getLong("user_code"));
+                System.out.println("user name: " + resultSet.getString("user_name"));
+                System.out.println("email address: " + resultSet.getString("email"));
+                System.out.println("password: " + resultSet.getString("password"));
+                System.out.println("age: " + resultSet.getInt("age"));
+                System.out.println("education: " + resultSet.getString("education"));
+            }
 
     } catch (ClassNotFoundException e)
     {
